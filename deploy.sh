16096d8ca6afbcc477cb783ac7b00d7eb04962f5
Sergio León <sergioleon4004@hotmail.com>
2022-08-23 20:41:16 -0500

    Build: updated deploy

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git init
 git checkout -b main
 git add .
 git add -A
-git commit -m 'deploy'
+git commit -am 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
