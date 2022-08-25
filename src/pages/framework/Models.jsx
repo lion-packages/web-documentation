cbe42100436162fe5854de60697f4b8aa671cf33
Sergio León <sergioleon4004@hotmail.com>
2022-08-25 09:59:08 -0500

    Feat: Framework/Models module has been created

--- src/pages/framework/Models.jsx
+++ src/pages/framework/Models.jsx
@@ -4,7 +4,42 @@ import Badge from "react-bootstrap/Badge";
 import Alert from "react-bootstrap/Alert";
 
 function Models() {
-    return <h1>Models</h1>;
+  return (
+    <>
+      <div className="mb-3">
+        <h4>Models</h4>
+
+        <p>
+          You can create models from the command line{" "}
+          <Badge bg={"secondary"}>
+            php lion new:model model_name
+          </Badge>.
+        </p>
+      </div>
+
+      <div className="mb-3">
+        <h4>Example #1</h4>
+
+        <CodeBlock
+          language={'powershell'}
+          content={'php lion new:model HomeModel'}
+        />
+
+        <CodeBlock
+          langueage={"php"}
+          content={
+            "<?php\n\n" +
+            "namespace App\\Http\\Models;\n\n" +
+            "use LionSql\\Drivers\\MySQLDriver as Builder;\n\n" +
+            "class HomeModel {\n\n" +
+            "\tpublic function __contruct() {\n\n" +
+            "\t}\n\n" +
+            "}"
+          }
+        />
+      </div>
+    </>
+  );
 }
 
 export default Models;
