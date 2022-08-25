b97b45e1e405a12c96b20fc09ba1e67366bf4e44
Sergio León <sergioleon4004@hotmail.com>
2022-08-25 09:19:19 -0500

    Feat: Framework/Controller module has been created

--- src/pages/framework/Controllers.jsx
+++ src/pages/framework/Controllers.jsx
@@ -4,7 +4,41 @@ import Badge from "react-bootstrap/Badge";
 import Alert from "react-bootstrap/Alert";
 
 function Controllers() {
-    return <h1>Controllers</h1>;
+  return (
+    <>
+      <div className="mb-3">
+        <h4>Controllers</h4>
+
+        <p>
+          You can create controllers from the command line{" "}
+          <Badge bg={"secondary"}>
+            php lion new:controller controller_name
+          </Badge>.
+        </p>
+      </div>
+
+      <div className="mb-3">
+        <h4>Example #1</h4>
+
+        <CodeBlock
+          language={'powershell'}
+          content={'php lion new:controller HomeController'}
+        />
+
+        <CodeBlock
+          langueage={"php"}
+          content={
+            "<?php\n\n" +
+            "namespace App\\Http\\Controllers;\n\n" +
+            "class HomeController {\n\n" +
+            "\tpublic function __contruct() {\n\n" +
+            "\t}\n\n" +
+            "}"
+          }
+        />
+      </div>
+    </>
+  );
 }
 
 export default Controllers;
--- src/pages/framework/Routes.jsx
+++ src/pages/framework/Routes.jsx
@@ -4,7 +4,34 @@ import Badge from "react-bootstrap/Badge";
 import Alert from "react-bootstrap/Alert";
 
 function Routes() {
-    return <h1>Routes</h1>;
+  return (
+    <>
+      <div className="mb-3">
+        <h4>Routes</h4>
+
+        <p>
+          <Link to={"/libraries/route"}>Lion-Route</Link> has been implemented
+          for route handling. More information at{" "}
+          <Link to={"/libraries/route"}>Lion-Route</Link>, from the web you can
+          add all the necessary routes for the operation of your web application{" "}
+          <Badge bg={"secondary"}>routes/web.php</Badge>.
+        </p>
+
+        <CodeBlock
+          langueage={"php"}
+          content={
+            "<?php\n\n" +
+            "use App\\Http\\Controllers\\HomeController;\n\n" +
+            "Route::get('/', [HomeController::class, 'index']);\n\n" +
+            "// or\n\n" +
+            "Route::get('/', function() {\n" +
+            "\t(new HomeController())->index();\n" +
+            "});"
+          }
+        />
+      </div>
+    </>
+  );
 }
 
 export default Routes;
