654da700a92def59558533520eeeda3821a876b5
Sergio León <sergioleon4004@hotmail.com>
2022-08-28 11:15:58 -0500

    Updated docs

---
+++ src/pages/components/ListCommands.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import CodeBlock from "./CodeBlock";
+import Table from "react-bootstrap/Table";
+
+function ListCommands() {
+  const [listCommands, setListCommands] = useState([
+    {
+      description: "Created command to start server locally",
+      command: "php lion serve",
+    },
+    {
+      description: "Command to create run unit tests",
+      command: "php lion new:test name_test",
+    },
+    {
+      description: "Command required for the creation of new Capsules",
+      command: "php lion new:capsule name_capsule",
+    },
+    {
+      description: "Command required for the creation of new Commands",
+      command: "php lion new:command name_command",
+    },
+    {
+      description: "Command required for the creation of new Controllers",
+      command: "php lion new:controller name_controller",
+    },
+    {
+      description: "Command required for the creation of new Middleware",
+      command: "php lion new:middleware name_middleware",
+    },
+    {
+      description: "Command required for the creation of new Middleware",
+      command: "php lion new:middleware name_middleware",
+    },
+    {
+      description: "Command required for the creation of new Models",
+      command: "php lion new:model name_model",
+    },
+    {
+      description: "Command required for rule creation",
+      command: "php lion new:rule name_rule",
+    },
+    {
+      description: "Created command to generate JWT token",
+      command: "php lion token:jwt",
+    },
+    {
+      description: "Command to view a list of available web routes",
+      command: "php lion route:list",
+    },
+    {
+      description: "Command to create public and private keys with RSA",
+      command: "php lion key:rsa",
+    },
+  ]);
+
+  return (
+    <>
+      <Table striped size="sm" variant="dark">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Command</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listCommands.map((command) => (
+            <tr>
+              <td>{command.description}</td>
+              <td>{command.command}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
+}
+
+export default ListCommands;
--- src/pages/framework/Commands.jsx
+++ src/pages/framework/Commands.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import CodeBlock from "../components/CodeBlock";
 import Badge from "react-bootstrap/Badge";
 import Alert from "react-bootstrap/Alert";
+import ListCommands from "../components/ListCommands";
 
 function Commands() {
   return (
@@ -11,26 +12,10 @@ function Commands() {
 
         <p>
           More information about the use of internal commands{" "}
-          <Link to={"/libraries/command"}>Lion-Command</Link>.
+          <Link to={"/libraries/command"}>Lion-Command</Link>
         </p>
 
-        <CodeBlock
-          language={"powershell"}
-          content={
-            "php lion serve\n" +
-            "php lion new:controller name_controller\n" +
-            "php lion new:model name_model\n" +
-            "php lion new:middleware name_middleware\n" +
-            "php lion new:command name_command\n" +
-            "php lion new:capsule name_capsule\n" +
-            "php lion new:test name_test\n" +
-            "php lion new:rule name_rule\n" +
-            "php lion key:rsa\n" +
-            "php lion test\n" +
-            "php lion token:jwt\n" +
-            "php lion route:list\n"
-          }
-        />
+        <ListCommands />
       </div>
 
       <hr />
@@ -61,7 +46,8 @@ function Commands() {
         <CodeBlock language={"powershell"} content={"php lion route:list"} />
 
         <Alert variant={"warning"}>
-          Note: The routes are loaded with the server route <Badge bg="warning">SERVER_URL</Badge> set in{" "}
+          Note: The routes are loaded with the server route{" "}
+          <Badge bg="warning">SERVER_URL</Badge> set in{" "}
           <Badge bg="warning">.env</Badge>, modify this route to avoid errors in
           the execution of the process, in the file{" "}
           <Badge bg="warning">public/index.php</Badge> there is a public route
