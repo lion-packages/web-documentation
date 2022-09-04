d107f76924c365ea28b865fbbbde00599f6d8559
Sergio León <sergioleon4004@hotmail.com>
2022-09-03 20:31:46 -0500

    Feat: added form

--- src/pages/DashboardContent.jsx
+++ src/pages/DashboardContent.jsx
@@ -1,12 +1,32 @@
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row, Form, InputGroup } from "react-bootstrap";
 import { FaReact, FaVuejs, FaGithub } from "react-icons/fa";
 import { DiPhp } from "react-icons/di";
 import { LinkContainer } from "react-router-bootstrap";
 import { FiArrowRight } from "react-icons/fi";
 import { RiTestTubeFill } from "react-icons/ri";
 import codeDashboard from "../assets/img/code-snap.png";
+import CodeBlock from "./components/CodeBlock";
+import { useState } from "react";
+import axios from "axios";
 
 function DashboardContent() {
+  const [response, setResponse] = useState({ status: "", message: "" });
+  const [text, setText] = useState("");
+  const [time, setTime] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const start = Date.now();
+
+    axios
+      .post("https://lion-client-php.herokuapp.com/new-text", { text: text })
+      .then((res) => {
+        setResponse({ status: res.data.status, message: res.data.message });
+        setTime(Math.abs(start - Date.now()));
+        console.log(typeof time)
+      });
+  };
+
   return (
     <>
       <Container className="my-5">
@@ -142,6 +162,79 @@ function DashboardContent() {
                 <img src={codeDashboard} className={"rounded img-fluid"} />
               </Col>
             </Row>
+          </Col>
+        </Row>
+      </Container>
+
+      <hr className="text-white" />
+
+      <Container className="my-5">
+        <h3 className="mt-5 mb-0 text-degradient text-center">SEND DATA</h3>
+
+        <Row className="text-white">
+          <Col xs={12} sm={12} md={5} lg={5} xl={5}>
+            <Form
+              onSubmit={handleSubmit}
+              className="border border-secondary rounded p-4 mt-5"
+            >
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Write something here</Form.Label>
+
+                <InputGroup className="mb-3">
+                  <Form.Control
+                    type="text"
+                    placeholder="hello world..."
+                    onChange={(e) => setText(e.target.value)}
+                    autoComplete="off"
+                    required
+                  />
+
+                  <Button type="submit" variant="outline-primary">
+                    SEND
+                  </Button>
+                </InputGroup>
+              </Form.Group>
+            </Form>
+          </Col>
+
+          <Col xs={12} sm={12} md={7} lg={7} xl={7} className="mt-5">
+            <h5>Response JSON</h5>
+
+            <CodeBlock
+              language={"json"}
+              content={
+                `// time: ${time} ms\n\n` +
+                "{\n" +
+                `\tstatus: '${response.status}'\n` +
+                `\tmessage: '${response.message}'\n` +
+                "}"
+              }
+            />
+
+            <hr />
+
+            <h5>Javascript/ReactJS</h5>
+
+            <CodeBlock
+              language={"php"}
+              content={
+                "// use with POSTMAN\n\n" +
+                "axios.post('https://lion-client-php.herokuapp.com/new-text', { text: text }).then(res => {\n" +
+                "\tconsole.log(res.data);\n" +
+                "});"
+              }
+            />
+
+            <h5>Lion-Framework PHP</h5>
+
+            <CodeBlock
+              language={"php"}
+              content={
+                "<?php\n\n" +
+                "use LionRoute\\Route;\n\n" +
+                "Route::post('new-text', fn() => response->success(request->text));"
+              }
+            />
           </Col>
         </Row>
       </Container>
