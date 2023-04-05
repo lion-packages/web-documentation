d325f9d1797c4fe1c65ce9477db7f229ca50a928
Sergio León <sergioleon4004@hotmail.com>
2023-04-05 16:21:50 -0500

    feat: added documentation for route library

--- src/Tools/Content.jsx
+++ src/Tools/Content.jsx
@@ -25,20 +25,29 @@ import v13_5_0_FRM from "./Versions/Framework/v13_5_0_FRM";
 import v4_7_0_LF from "./Versions/Libraries/LionFiles/v4_7_0_LF";
 import v4_8_0_LF from "./Versions/Libraries/LionFiles/v4_8_0_LF";
 import v4_9_0_LF from "./Versions/Libraries/LionFiles/v4_9_0_LF";
+
 import v2_1_2_LH from "./Versions/Libraries/LionHelpers/v2_1_2_LH";
 import v2_1_3_LH from "./Versions/Libraries/LionHelpers/v2_1_3_LH";
+
 import v4_0_0_LM from "./Versions/Libraries/LionMailer/v4_0_0_LM";
 import v4_2_0_LM from "./Versions/Libraries/LionMailer/v4_2_0_LM";
+
 import v5_3_0_LR from "./Versions/Libraries/LionRequest/v5_3_0_LR";
+
 import v8_1_0_LRT from "./Versions/Libraries/LionRoute/v8_1_0_LRT";
 import v8_2_0_LRT from "./Versions/Libraries/LionRoute/v8_2_0_LRT";
 import v8_3_0_LRT from "./Versions/Libraries/LionRoute/v8_3_0_LRT";
+import v8_3_1_LRT from "./Versions/Libraries/LionRoute/v8_3_1_LRT";
+import v8_3_2_LRT from "./Versions/Libraries/LionRoute/v8_3_2_LRT";
+
 import v6_10_0_LS from "./Versions/Libraries/LionSecurity/v6_10_0_LS";
 import v6_10_1_LS from "./Versions/Libraries/LionSecurity/v6_10_1_LS";
 import v6_9_0_LS from "./Versions/Libraries/LionSecurity/v6_9_0_LS";
 import v6_11_0_LS from "./Versions/Libraries/LionSecurity/v6_11_0_LS";
+
 import v1_4_0_LSP from "./Versions/Libraries/LionSpreadsheet/v1_4_0_LSP";
 import v1_5_0_LSP from "./Versions/Libraries/LionSpreadsheet/v1_5_0_LSP";
+
 import v6_4_0_LSQ from "./Versions/Libraries/LionSQL/v6_4_0_LSQ";
 import v6_5_1_LSQ from "./Versions/Libraries/LionSQL/v6_5_1_LSQ";
 import v6_6_0_LSQ from "./Versions/Libraries/LionSQL/v6_6_0_LSQ";
@@ -47,6 +56,7 @@ import v7_1_0_LSQ from "./Versions/Libraries/LionSQL/v7_1_0_LSQ";
 import v7_2_0_LSQ from "./Versions/Libraries/LionSQL/v7_2_0_LSQ";
 import v7_2_1_LSQ from "./Versions/Libraries/LionSQL/v7_2_1_LSQ";
 import v7_3_0_LSQ from "./Versions/Libraries/LionSQL/v7_3_0_LSQ";
+
 import v1_0_0_LA from "./Versions/Libraries/LionAuthentication/v1_0_0_LA";
 
 export default function Content(mdText = "") {
@@ -210,6 +220,8 @@ export default function Content(mdText = "") {
           description:
             "This library has a quick use of the router with regular expressions based on mrjgreen's phroute.",
           class: {
+            "v8.3.2": v8_3_2_LRT(),
+            "v8.3.1": v8_3_1_LRT(),
             "v8.3.0": v8_3_0_LRT(),
             "v8.2.0": v8_2_0_LRT(),
             "v8.1.0": v8_1_0_LRT(),
