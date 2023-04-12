5f78a12fd9f4dee4472b434c1f70e74807bc8bec
Sergio León <sergioleon4004@hotmail.com>
2023-04-11 22:21:28 -0500

    feat: added plugin to generate PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: "./src/assets/512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any image",
+          },
+        ],
+      },
+    }),
+  ],
+});
