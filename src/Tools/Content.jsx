221105d45736c9311d8f2e354800d0f449fbbcf5
Sergio León <sergioleon4004@hotmail.com>
2023-04-24 20:44:20 -0500

    feat: added documentation for framework

--- src/Tools/Content.jsx
+++ src/Tools/Content.jsx
@@ -18,6 +18,7 @@ import v13_9_0_CMMD from "./Versions/Commands/v13_9_0_CMMD";
 import v13_10_0_CMMD from "./Versions/Commands/v13_10_0_CMMD";
 import v13_11_0_CMMD from "./Versions/Commands/v13_11_0_CMMD";
 import v13_12_0_CMMD from "./Versions/Commands/v13_12_0_CMMD";
+import v13_13_0_CMMD from "./Versions/Commands/v13_13_0_CMMD";
 
 // framework
 import v12_16_1_FRM from "./Versions/Framework/v12_16_1_FRM";
@@ -36,6 +37,7 @@ import v13_9_0_FRM from "./Versions/Framework/v13_9_0_FRM";
 import v13_10_0_FRM from "./Versions/Framework/v13_10_0_FRM";
 import v13_11_0_FRM from "./Versions/Framework/v13_11_0_FRM";
 import v13_12_0_FRM from "./Versions/Framework/v13_12_0_FRM";
+import v13_13_0_FRM from "./Versions/Framework/v13_13_0_FRM";
 
 // libraries
 import v4_7_0_LF from "./Versions/Libraries/LionFiles/v4_7_0_LF";
@@ -81,6 +83,7 @@ import v1_0_0_LA from "./Versions/Libraries/LionAuthentication/v1_0_0_LA";
 export default function Content(mdText = "") {
   return {
     commands: {
+      "v13.13.0": v13_13_0_CMMD(),
       "v13.12.0": v13_12_0_CMMD(),
       "v13.11.0": v13_11_0_CMMD(),
       "v13.10.0": v13_10_0_CMMD(),
@@ -99,6 +102,7 @@ export default function Content(mdText = "") {
       "v12.16.1": v12_16_1_CMMD(),
     },
     framework: {
+      "v13.13.0": v13_13_0_FRM(mdText),
       "v13.12.0": v13_12_0_FRM(mdText),
       "v13.11.0": v13_11_0_FRM(mdText),
       "v13.10.0": v13_10_0_FRM(mdText),
