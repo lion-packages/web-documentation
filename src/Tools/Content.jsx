25cbefc009822f13920989cff79d3cdd1c7a3331
Sergio León <sergioleon4004@hotmail.com>
2023-06-15 16:42:19 -0500

    feat: framework v14.17.1 documentation has been added

--- src/Tools/Content.jsx
+++ src/Tools/Content.jsx
@@ -2,7 +2,6 @@
 import Library from "../pages/Library";
 
 //commands
-import v14_9_0_CMMD from "./Versions/Commands/v14_9_0_CMMD";
 import v14_10_0_CMMD from "./Versions/Commands/v14_10_0_CMMD";
 import v14_10_1_CMMD from "./Versions/Commands/v14_10_1_CMMD";
 import v14_11_0_CMMD from "./Versions/Commands/v14_11_0_CMMD";
@@ -12,9 +11,9 @@ import v14_14_0_CMMD from "./Versions/Commands/v14_14_0_CMMD";
 import v14_15_0_CMMD from "./Versions/Commands/v14_15_0_CMMD";
 import v14_16_0_CMMD from "./Versions/Commands/v14_16_0_CMMD";
 import v14_17_0_CMMD from "./Versions/Commands/v14_17_0_CMMD";
+import v14_17_1_CMMD from "./Versions/Commands/v14_17_1_CMMD";
 
 // framework
-import v14_9_0_FRM from "./Versions/Framework/v14_9_0_FRM";
 import v14_10_0_FRM from "./Versions/Framework/v14_10_0_FRM";
 import v14_10_1_FRM from "./Versions/Framework/v14_10_1_FRM";
 import v14_11_0_FRM from "./Versions/Framework/v14_11_0_FRM";
@@ -24,6 +23,7 @@ import v14_14_0_FRM from "./Versions/Framework/v14_14_0_FRM";
 import v14_15_0_FRM from "./Versions/Framework/v14_15_0_FRM";
 import v14_16_0_FRM from "./Versions/Framework/v14_16_0_FRM";
 import v14_17_0_FRM from "./Versions/Framework/v14_17_0_FRM";
+import v14_17_1_FRM from "./Versions/Framework/v14_17_1_FRM";
 
 // libraries
 import v4_7_0_LF from "./Versions/Libraries/LionFiles/v4_7_0_LF";
@@ -77,6 +77,7 @@ import v1_0_0_LA from "./Versions/Libraries/LionAuthentication/v1_0_0_LA";
 export default function Content(mdText = "") {
   return {
     commands: {
+      "v14.17.1": v14_17_1_CMMD(),
       "v14.17.0": v14_17_0_CMMD(),
       "v14.16.0": v14_16_0_CMMD(),
       "v14.15.0": v14_15_0_CMMD(),
@@ -86,7 +87,7 @@ export default function Content(mdText = "") {
       "v14.11.0": v14_11_0_CMMD(),
       "v14.10.1": v14_10_1_CMMD(),
       "v14.10.0": v14_10_0_CMMD(),
-      "v14.9.0": v14_9_0_CMMD(),
+      // "v14.9.0": v14_9_0_CMMD(),
       // "v14.8.0": v14_8_0_CMMD(),
       // "v14.7.0": v14_7_0_CMMD(),
       // "v14.6.0": v14_6_0_CMMD(),
@@ -118,6 +119,7 @@ export default function Content(mdText = "") {
       // "v12.16.1": v12_16_1_CMMD(),
     },
     framework: {
+      "v14.17.1": v14_17_1_FRM(mdText),
       "v14.17.0": v14_17_0_FRM(mdText),
       "v14.16.0": v14_16_0_FRM(mdText),
       "v14.15.0": v14_15_0_FRM(mdText),
@@ -127,7 +129,7 @@ export default function Content(mdText = "") {
       "v14.11.0": v14_11_0_FRM(mdText),
       "v14.10.1": v14_10_1_FRM(mdText),
       "v14.10.0": v14_10_0_FRM(mdText),
-      "v14.9.0": v14_9_0_FRM(mdText),
+      // "v14.9.0": v14_9_0_FRM(mdText),
       // "v14.8.0": v14_8_0_FRM(mdText),
       // "v14.7.0": v14_7_0_FRM(mdText),
       // "v14.6.0": v14_6_0_FRM(mdText),
