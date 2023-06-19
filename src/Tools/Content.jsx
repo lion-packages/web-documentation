452f86aa7d5df618d97b6136215e14e2e0b60a98
Sergio León <sergioleon4004@hotmail.com>
2023-06-18 23:43:30 -0500

    feat: documentation for sql library v8.6.0 has been added

--- src/Tools/Content.jsx
+++ src/Tools/Content.jsx
@@ -59,7 +59,6 @@ import v1_4_0_LSP from "./Versions/Libraries/LionSpreadsheet/v1_4_0_LSP";
 import v1_5_0_LSP from "./Versions/Libraries/LionSpreadsheet/v1_5_0_LSP";
 import v1_6_0_LSP from "./Versions/Libraries/LionSpreadsheet/v1_6_0_LSP";
 
-import v8_0_0_LSQ from "./Versions/Libraries/LionSQL/v8_0_0_LSQ";
 import v8_0_1_LSQ from "./Versions/Libraries/LionSQL/v8_0_1_LSQ";
 import v8_0_2_LSQ from "./Versions/Libraries/LionSQL/v8_0_2_LSQ";
 import v8_0_3_LSQ from "./Versions/Libraries/LionSQL/v8_0_3_LSQ";
@@ -69,6 +68,7 @@ import v8_2_0_LSQ from "./Versions/Libraries/LionSQL/v8_2_0_LSQ";
 import v8_3_0_LSQ from "./Versions/Libraries/LionSQL/v8_3_0_LSQ";
 import v8_4_0_LSQ from "./Versions/Libraries/LionSQL/v8_4_0_LSQ";
 import v8_5_0_LSQ from "./Versions/Libraries/LionSQL/v8_5_0_LSQ";
+import v8_6_0_LSQ from "./Versions/Libraries/LionSQL/v8_6_0_LSQ";
 
 import v5_3_0_LR from "./Versions/Libraries/LionRequest/v5_3_0_LR";
 
@@ -175,6 +175,7 @@ export default function Content(mdText = "") {
           description:
             "Package responsible for generating and executing MySQL processes.",
           class: {
+            "v8.6.0": v8_6_0_LSQ(),
             "v8.5.0": v8_5_0_LSQ(),
             "v8.4.0": v8_4_0_LSQ(),
             "v8.3.0": v8_3_0_LSQ(),
@@ -184,7 +185,7 @@ export default function Content(mdText = "") {
             "v8.0.3": v8_0_3_LSQ(),
             "v8.0.2": v8_0_2_LSQ(),
             "v8.0.1": v8_0_1_LSQ(),
-            "v8.0.0": v8_0_0_LSQ(),
+            // "v8.0.0": v8_0_0_LSQ(),
             // "v7.7.0": v7_7_0_LSQ(),
             // "v7.6.0": v7_6_0_LSQ(),
             // "v7.5.0": v7_5_0_LSQ(),
