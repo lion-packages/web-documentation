9889443066ba508c143b0e9282903559ce9e34e4
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-11-06 12:03:07 -0500

    add component infoDataCart and setting responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import CardInfo from './components/CardInfo'
+import { infoDataCart } from '../infoData'
 
 const Home = () => {
   return (
     <div>
       <section className='flex flex-col gap-4 items-center'>
-        <div className='flex flex-col text-6xl italic tracking-tighter text-center opacity-100 font-medium text-white'>
+        <div className='flex flex-col text-3xl lg:text-6xl italic tracking-tighter text-center opacity-100 font-medium text-white'>
           <span className='p-2 text-title-principal '>Boost your PHP development</span>
           <span className='p-2 text-title-principal '>With the Lion-Framework</span>
         </div>
         <p className='text-clr-secundary text-lg text-center max-w-prose '>
-          can also serve as an API backend for a JavaScript single page application or a mobile application.
+          Can also serve as an API backend for a JavaScript single page application or a mobile application.
         </p>
 
         <div className='flex gap-2'>
@@ -20,35 +21,8 @@ const Home = () => {
       </section>
 
       <section className='w-full h-full bg-gradient-to-r from-dark-100 via-dark-300 to-dark-100'>
-        <article className='flex flex-col md:grid md:grid-cols-3 items-center m-16 p-8 gap-3'>
-          <a href="#" className="block max-w-sm p-6 bg-dark-200 border-separate rounded-xl shadow-2xl">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Noteworthy technology acquisitions 2021</h5>
-            <p className="font-normal text-clr-secundary ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-          </a>
-
-
-          <a href="#" className="block max-w-sm p-6 bg-dark-200 border-separate rounded-xl shadow-2xl">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Noteworthy technology acquisitions 2021</h5>
-            <p className="font-normal text-clr-secundary ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-          </a>
-
-
-          <a href="#" className="block max-w-sm p-6 bg-dark-200 border-separate rounded-xl shadow-2xl">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Noteworthy technology acquisitions 2021</h5>
-            <p className="font-normal text-clr-secundary ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-          </a>
-          <a href="#" className="block max-w-sm p-6 bg-dark-200 border-separate rounded-xl shadow-2xl">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Noteworthy technology acquisitions 2021</h5>
-            <p className="font-normal text-clr-secundary ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-          </a>
-          <a href="#" className="block max-w-sm p-6 bg-dark-200 border-separate rounded-xl shadow-2xl">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Noteworthy technology acquisitions 2021</h5>
-            <p className="font-normal text-clr-secundary ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-          </a>
-          <a href="#" className="block max-w-sm p-6 bg-dark-200 border-separate rounded-xl shadow-2xl">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Noteworthy technology acquisitions 2021</h5>
-            <p className="font-normal text-clr-secundary ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-          </a>
+        <article className='flex flex-col md:grid md:grid-cols-3 items-center m-16 p-8 gap-5'>
+          {infoDataCart.map((data, index) => <CardInfo key={index} {...data} />)}
         </article>
       </section>
     </div>
