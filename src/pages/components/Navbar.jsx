ef8f6471f04fd001a7d5b3aa5fa461053df891f4
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-11-06 12:12:54 -0500

    feat(Root): navbar fix responsiv

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,30 +1,44 @@
-import React from 'react'
-import { AiOutlineMail } from "react-icons/ai";
-import { FaGithub, FaYoutube } from "react-icons/fa"
-import logo from "./../../assets/img/Lion_black_icon_long.png";
+import { AiOutlineMail } from 'react-icons/ai';
+import { FaGithub, FaYoutube, FaBars } from 'react-icons/fa';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from './../../assets/img/Lion_black_icon_long.png';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <nav className="bg-dark-100 py-4 px-20 flex items-center justify-between">
+      <nav className="bg-dark-100 flex items-center justify-between px-4 py-2 lg:py-4 lg:px-20">
         <div className="px-2">
-          <img src={logo} width={154} height={56} />
+          <Link to='/' >
+            <img src={logo} width={148} height={56} />
+          </Link>
         </div>
-        <div className="flex gap-2 text-clr-secundary font-semibold ">
+        <div className="md:hidden">
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <FaBars className="text-gray-400" />
+          </button>
+        </div>
+        <div className={`md:flex gap-1 text-clr-secundary font-semibold ${isOpen ? 'flex-col' : 'hidden'} md:flex-row items-center`}>
           <div className='flex gap-2 justify-center items-center'>
-            <button className='underline underline-offset-2 decoration-primary-100 hover:decoration-gray-100'>Framework</button>
-            <button className='underline underline-offset-2 decoration-primary-100 hover:decoration-gray-100'>Packages</button>
+            <button className='underline underline-offset-2 decoration-primary-100 hover:decoration-gray-100'>
+              Framework
+            </button>
+            <button className='underline underline-offset-2 decoration-primary-100 hover:decoration-gray-100'>
+              Packages
+            </button>
           </div>
 
-          <div className='flex gap-2 justify-center items-center'>
+          <div className='flex gap-2 px-3 justify-center items-center'>
             <a href="https://www.youtube.com/channel/UCJBOJVKok44BZt0kKlBeZyA" target='_blank' >
-              <FaYoutube size={"1.5em"} />
+              <FaYoutube size={"1.5em"} className='hover:text-primary-100' />
             </a>
             <a href="https://github.com/Sleon4" target='_blank' >
-              <FaGithub size={"1.5em"} />
+              <FaGithub size={"1.5em"} className='hover:text-primary-100' />
             </a>
             <a href="mailto:sergioleon4004@hotmail.com" target='_blank'>
-              <AiOutlineMail size={"1.5em"} />
+              <AiOutlineMail size={"1.5em"} className='hover:text-primary-100' />
             </a>
           </div>
         </div>
@@ -33,4 +47,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
