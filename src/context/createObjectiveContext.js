497035c7d654323115105dbf645bf00c93576af7
JuanVaquiro <jvaquiro23@itfip.edu.co>
2024-02-13 13:43:08 -0500

    refactor(Root): delet file

--- src/context/createObjectiveContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from 'react'
-
-export const CreateObjectiveContext = createContext(null)
-
-export const useCreateObjective = () => {
-  return useContext(CreateObjectiveContext)
-}
