b1c7da5b4e3ecc8964ecce2771c6f908067f8d14
JuanVaquiro <jvaquiro23@itfip.edu.co>
2024-02-13 10:29:55 -0500

    freat: modific import json data

--- src/containers/LayoutSider.jsx
+++ src/containers/LayoutSider.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { Outlet as Page } from 'react-router-dom'
+import { docs } from '../hooks/Tools/Framework/framework-v1.json'
 import Sidebar from '../pages/components/Sidebar'
 import NavbarSecundary from '../pages/components/NavbarSecundary'
 
 const LayoutSider = () => {
   return (
     <div className='flex'>
-    <Sidebar />
-    <div className='flex flex-col w-full'>
-      <div className='md:ml-64 mt-14 md:mt-24'>
-        <NavbarSecundary />
-      </div>
-      <div className='md:ml-64'>
-        <Page />
+      <Sidebar docs={docs} />
+      <div className='flex flex-col w-full'>
+        <div className='md:ml-64 mt-14 md:mt-24'>
+          <NavbarSecundary />
+        </div>
+        <div className='md:ml-64'>
+          <Page />
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
