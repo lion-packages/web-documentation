fb457d4fa7f9e27a1a9af1b66f0338e0fdb9bc63
JuanVaquiro <jvaquiro23@itfip.edu.co>
2024-04-15 09:38:29 -0500

    feat(Root): fix margin

--- src/containers/LayoutSider.jsx
+++ src/containers/LayoutSider.jsx
@@ -1,21 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../pages/components/Sidebar";
-import NavbarSecundary from "../pages/components/NavbarSecundary";
 import { useModules } from "../hooks/Context/ModulesContext";
 
 const LayoutSider = () => {
   const { loading } = useModules();
 
   return !loading ? (
-    <div className="flex">
+    <div className="">
       <Sidebar />
-
       <div className="flex flex-col w-full">
-        <div className="md:ml-64 mt-14 md:mt-24">
-          <NavbarSecundary />
-        </div>
-
-        <div className="md:ml-64">
+        <div className="md:ml-64 mt-16">
           <Outlet />
         </div>
       </div>
