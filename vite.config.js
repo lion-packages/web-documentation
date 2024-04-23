46017d05e399ef89b86c6409ddf6d8dca1c1e11a
Sleon4 <sergioleon4004@hotmail.com>
2024-04-23 09:37:48 -0500

    ci: Docker configuration has been updated

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      manifest: {
-        name: "My Awesome App",
-        short_name: "MyApp",
-        description: "My Awesome App description",
-        theme_color: "#111111",
-        icons: [
-          {
-            src: "/public/vite.svg",
-            sizes: "144x144",
-            type: "image/svg+xml",
-            purpose: "any",
-          },
-          {
-            src: "/public/vite.svg",
-            sizes: "192x192",
-            type: "image/svg+xml",
-            purpose: "any",
-          },
-          {
-            src: "/public/vite.svg",
-            sizes: "512x512",
-            type: "image/svg+xml",
-            purpose: "any",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
 });
