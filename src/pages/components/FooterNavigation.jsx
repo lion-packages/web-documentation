0d30485e5abdc52a8d99f2e152f67d128fb725a6
Sleon4 <sergioleon4004@hotmail.com>
2024-04-24 17:14:32 -0500

    style: footer style has been modified

--- src/pages/components/FooterNavigation.jsx
+++ src/pages/components/FooterNavigation.jsx
@@ -1,16 +1,10 @@
-import { AiFillGithub } from "react-icons/ai";
-import { RiMailSendFill } from "react-icons/ri";
-
 function FooterNavigation() {
   return (
     <footer
       className="footer mt-auto py-3 bg-dark-logo border-top border-secondary"
       style={{ "--bs-border-opacity": ".5" }}
     >
-      <p
-        className="text-center text-body-secondary text-white"
-        style={{ fontSize: 13.5 }}
-      >
+      <p className="text-center text-light">
         Released under the{" "}
         <a
           href="https://opensource.org/license/mit/"
@@ -22,12 +16,7 @@ function FooterNavigation() {
         .
       </p>
 
-      <p
-        className="text-center text-body-secondary text-white"
-        style={{ fontSize: 14.5 }}
-      >
-        Copyright © 2022-2023 Sergio León
-      </p>
+      <p className="text-center text-light">Copyright © Lion-Packages</p>
     </footer>
   );
 }
