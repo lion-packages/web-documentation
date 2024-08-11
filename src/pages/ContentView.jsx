d7fbb9478e4abfa8b0af252cb60ea111ef53adf6
Sleon4 <sergioleon4004@hotmail.com>
2024-08-11 11:47:21 -0500

    docs: lion/framework v3 documentation added

--- src/pages/ContentView.jsx
+++ src/pages/ContentView.jsx
@@ -9,37 +9,37 @@ export default function ContentView() {
   const [loading, setLoading] = useState(true);
   const [content, setContent] = useState(null);
 
+  const fetchData = () => {
+    const contentData = Content();
+
+    if (!contentData) {
+      setContent(<NotFound />);
+
+      return;
+    }
+
+    let item;
+
+    if (null === library) {
+      item = contentData.framework?.[item_version]?.docs?.[tab]?.list?.[code];
+    } else {
+      item =
+        contentData.library?.[library]?.[item_version]?.[tab]?.list?.[code];
+    }
+
+    if (item) {
+      setContent(item.code);
+    } else {
+      setContent(<NotFound />);
+    }
+  };
+
   useEffect(() => {
     setLoading(true);
 
-    const fetchData = () => {
-      const contentData = Content();
-
-      if (!contentData) {
-        setContent(<NotFound />);
-
-        return;
-      }
-
-      let item;
-
-      if (null === library) {
-        item = contentData.framework?.[item_version]?.docs?.[tab]?.list?.[code];
-      } else {
-        item =
-          contentData.library?.[library]?.[item_version]?.[tab]?.list?.[code];
-      }
-
-      if (item) {
-        setContent(item.code);
-      } else {
-        setContent(<NotFound />);
-      }
-    };
-
     fetchData();
 
-    setTimeout(() => setLoading(false), 500);
+    setLoading(false);
   }, [item_version, tab, code, library]);
 
   return <div>{loading ? <ContentLoading /> : content}</div>;
